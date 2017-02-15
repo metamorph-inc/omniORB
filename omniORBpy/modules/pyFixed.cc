1d7a0c6bd84772fbea15d95ed9790c20912f9737
dgrisby <dgrisby@31ce16a7-c309-44a6-b7c4-d857baf782c7>
2017-02-15 23:36:21 +0000

    Suppress warnings.
    git-svn-id: https://svn.code.sf.net/p/omniorb/svn/branches/4_2@6339 31ce16a7-c309-44a6-b7c4-d857baf782c7

--- omniORBpy/modules/pyFixed.cc
+++ omniORBpy/modules/pyFixed.cc
@@ -246,6 +246,7 @@ extern "C" {
     case Py_NE: r = cmp != 0; break;
     case Py_GT: r = cmp >  0; break;
     case Py_GE: r = cmp >= 0; break;
+    default:    r = 0;
     };
     
     PyObject* r_o = r ? Py_True : Py_False;
